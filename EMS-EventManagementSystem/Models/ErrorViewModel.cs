1b2cc030531f49d7db56f6a91aaf2732f449df38
Abdul Mufeez <abdul_mufeez@outlook.com>
2021-10-03 12:11:23 +0500

    Add project files.

---
+++ EMS-EventManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EMS_EventManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
