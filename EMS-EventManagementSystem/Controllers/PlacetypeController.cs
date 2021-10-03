090680175076afae0c192e38a71a1b4b69e479cf
Abdul Mufeez <abdul_mufeez@outlook.com>
2021-10-03 13:52:45 +0500

    Added Placetype form and dashboard pag

---
+++ EMS-EventManagementSystem/Controllers/PlacetypeController.cs
@@ -0,0 +1,44 @@
+﻿using EMS_EventManagementSystem.Data;
+using EMS_EventManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMS_EventManagementSystem.Controllers
+{
+    public class PlacetypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PlacetypeController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public IActionResult CreatePlacetype()
+        {
+            return View("PlaceTypeForm", new Placetype());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Save(Placetype placeType)
+        {
+            if (!ModelState.IsValid) { CreatePlacetype(); /*return View("PlaceTypeForm", new Placetype());*/ }
+            else
+            {
+                placeType.CreatedAt = DateTime.Now;
+                _context.Add(placeType);
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
